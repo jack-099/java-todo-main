fcdd6d541583ac2f0e939a21e8b21d2c79739b57
jack <landforjk01@gmail.com>
2023-04-30 18:21:03 +0900

    one

---
+++ hajime.java
@@ -0,0 +1,30 @@
+package hajime;
+
+public class hajime {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("はじめまして");
+
+	}
+
+	private int num = 0;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+}
